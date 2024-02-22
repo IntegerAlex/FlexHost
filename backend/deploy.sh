037a53d2d17a6350abc83c8d4930d44707a45169
akshat <akshat@localhost.localdomain>
2024-02-22 10:18:46 +0530

    added ufw to setup.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,5 +1,4 @@
-!#/bin/bash
-
-npm install
-npm build
-python3 -m http.server
+cd "${1}" || exit 1
+cd "${2}" || exit 1
+cd dist || exit 1
+/usr/local/bin/http-server -p "${3}" > "${PWD}/my_server_log.txt"
